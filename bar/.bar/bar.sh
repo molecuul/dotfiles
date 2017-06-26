3b88389f3c9d0a88189ecf945f4158972f5d95aa
molecuul <jtstanley9@gmail.com>
2017-06-26 16:14:49 -0500

    Made everything simpler, removed scripts, vim, bar

--- bar/.bar/bar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# bar init hold
-
-font_def="creep:pixelsize=12"
-font_icon="Siji:pixelsize=12"
-fg_def="#FF999999"
-bg_def="#77333333"
-
-$(dirname $0)/info.sh | lemonbar -f "$font_def" -f "$font_icon" -F "$fg_def" -B "$bg_def"
--- scripts/.scripts/configure_displays.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# fixes some common problems with my displays
-
-xrandr --output LVDS1 --primary
